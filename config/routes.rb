4e014962a52f394296d63bb40605666e4928db0d
kyuuki <kyuuki.japan@gmail.com>
2023-05-07 04:25:18 +0000

    ルートページに本一覧へのリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "static_page#root"
   # root to: "static_page#root"  # 上記はこれの省略形
+
+  resources :books
 end
