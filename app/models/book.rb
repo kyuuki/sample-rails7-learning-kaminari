b300693795f92ec6041981c71eddd447da14762e
kyuuki <kyuuki.japan@gmail.com>
2023-05-07 04:01:52 +0000

    バリデーション追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+  # https://railsguides.jp/active_record_validations.html
+
+  validates :title, presence: true, length: { maximum: 40 }
+
+  # nil も空文字も入る可能性がある (フォーム経由の場合は実質 nil は入らない)
+  validates :author, length: { maximum: 20 }
+
+  # 西暦 0 年は存在しない
+  validates :publication_year, comparison: { other_than: 0 }, allow_nil: true
 end
