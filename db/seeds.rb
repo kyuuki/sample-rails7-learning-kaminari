894cbd48517958018419140e8e3b9c5a312b3515
kyuuki <kyuuki.japan@gmail.com>
2024-01-14 12:50:51 +0000

    ページネーション用に大量のデータを投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+for i in 1..100 do
+  Book.create(title: "Title #{i}", author: "Author #{i}", publication_year: 2000 + i)
+end
